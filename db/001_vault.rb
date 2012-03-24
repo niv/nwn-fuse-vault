15f0cc3c69f7b9ba3514bcf475add7beef4b7b14
niv <le@e-ix.net>
2012-03-24 13:50:05 +0100

    migrations: non-null columns

--- db/001_vault.rb
+++ db/001_vault.rb
@@ -3,12 +3,12 @@ Sequel.migration do
     create_table(:vault) do
       primary_key :id
 
-      String :account
-      String :filename
-      File :data
+      String :account, :null => false
+      String :filename, :null => false
+      File :data, :null => false
 
-      Time :created_on, :default => 'now()'
-      Time :updated_on
+      Time :created_on, :null => false
+      Time :updated_on, :null => false
     end
   end
 
